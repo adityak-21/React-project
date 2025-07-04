2a9e2b03b9fb2608fe927059a0c64d229ade05e9
Aditya Khandelwal <aditya.khandelwal@vmock.com>
2025-07-04 20:06:53 +0530

    Final Change for docker

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAccessToken } from "./AuthApi";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:8000/api/v1",
 });
 
 api.interceptors.request.use(
